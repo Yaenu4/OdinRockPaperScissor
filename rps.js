d23bc41ed8fb6d03bd02b20d413b98d2f4c8b358
Yannick <yannick.hayoz04@gmail.com>
2022-10-23 12:13:30 +0200

    Add game function plays 5 rounds, keeps track of score, decides winner at the end

--- rps.js
+++ rps.js
@@ -29,19 +29,18 @@ return choice;
 function playRound(playerSelection, computerSelection) {
 playerSelection = playerSelection.toLowerCase();
 computerSelection = getComputerChoice();
-
 switch(true) {
     case playerSelection === computerSelection :
-    return ("It's a tie! Play Again!");
+    return ("It's a tie!");
     break;
     case playerSelection === "rock" && computerSelection === "scissors" :
-    return ("You Win! Rock beats scissors!");
+    return ("You win! Rock beats scissors!");
     break;   
     case playerSelection === "rock" && computerSelection === "paper" :
     return ("You lose! Paper beats Rock!");
     break;
     case playerSelection === "scissors" && computerSelection === "paper" :
-    return("You Win! Scissors beats paper!");
+    return("You win! Scissors beats paper!");
     break;
     case playerSelection === "scissors" && computerSelection === "rock" :
     return ("You lose! Rock beats scissors!")
@@ -52,4 +51,48 @@ switch(true) {
     case playerSelection === "paper" && computerSelection === "scissors" :
     return ("You lose! Scissors beats paper!")            
 }
+}
+// 3. call playRound function
+// prompt get value playerSelection
+// jf return you lose + 0 point
+// if return you win + 1 point
+// for loop - i++ aslong I < 5
+// the end you have won the game by score1:Score2
+// if score 1 < score 2 you lost
+
+function game() {
+let score1 = 0;
+let score2 = 0;
+
+    for (let i = 0; i < 5; i++) {
+
+    let winorlose = playRound(prompt("What do you choose?"));
+  
+    switch(true) {
+
+   case winorlose.includes("win"):
+    console.log(winorlose);
+    score1 += 1;
+   console.log("Score: Player: " + score1 + " Computer: " +score2);
+   break;
+   case winorlose.includes("lose") :
+    console.log(winorlose);
+    score2 += 1;
+    console.log("Score: Player: " + score1 + " Computer: " +score2);
+    break;
+    case winorlose.includes("tie") :
+        console.log(winorlose);
+        console.log("Score: Player: " + score1 + " Computer: " +score2);
+    }
+}
+
+   switch(true) {
+    case score1 < score2 :
+    console.log("Final Score: Player: " + score1 + " Computer: " +score2);
+    console.log("You lost the game! Loser!");
+    break;
+    default:
+    console.log("Final Score: Player: " + score1 + " Computer: " +score2);
+    console.log("Congratulations! You won the game!")    
+   }
 }
